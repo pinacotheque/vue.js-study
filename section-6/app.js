ca49bc256ba1f589e3c15b0bd85bd37fe405b104
Gamze Güneri <gamzegunerig@gmail.com>
2021-10-25 23:16:31 +0300

    adds

---
+++ section-6/app.js
@@ -0,0 +1,49 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '01234 5678 991',
+                    email: 'manuel@localhost.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '09876 543 221',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
+    }
+})
+app.component('friend-contact', {
+    template: `
+        <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails" > {{ visible ? 'Hide' : 'Show' }} Details</button >
+        <ul v-if="visible">
+        <li><strong>Phone:</strong>{{ friend.phone }}</li>
+        <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+    </li>  
+    `,
+    data() {
+        return {
+            visible: false,
+            friend: {
+                id: 'julie',
+                name: 'Julie Jones',
+                phone: '09876 543 221',
+                email: 'julie@localhost.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.visible = !this.visible
+        }
+    }
+})
+app.mount('#app')
