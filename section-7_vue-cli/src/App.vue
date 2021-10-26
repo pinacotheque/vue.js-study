c48dd106024294995bf29f3bb03290644db018af
Gamze Güneri <gamzegunerig@gmail.com>
2021-10-26 12:45:07 +0300

    emit events

---
+++ section-7_vue-cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <section>
+        <header>
+            <h1>My Friends</h1>
+        </header>
+        <ul>
+            <friend-contact v-for="friend in friends" 
+            :key="friend.id" 
+            :id="friend.id"
+            :name="friend.name" 
+            :phone-number="friend.phone" 
+            :email-address="friend.email" 
+            :is-favorite="friend.isFavorite"
+            @toggle-fav="toggleFavStatus">
+            </friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            friends: [
+                {
+                    id: 'Gamze',
+                    name: 'Gamze Guneri',
+                    phone: '12345 567',
+                    email: 'gamze@gmail.com',
+                    isFavorite: true
+                }
+            ]
+        }
+    },
+    methods: {
+        toggleFavStatus(friendId) {
+           const selectedFriend = this.friends.find((friend) => friend.id === friendId)
+           selectedFriend.isFavorite = !selectedFriend.isFavorite
+        }
+    }
+}
+</script>
---
+++ section-7_vue-cli/src/components/FriendComponent.vue
@@ -0,0 +1,118 @@
+<template>
+    <li>
+        <h2>{{name}} {{isFavorite ? '(Fav)' : ''}} </h2>
+        <button @click="toggleDetails">{{visible ? 'Hide': 'Show'}} Details</button>
+        <button @click="toggleFav">{{isFavorite ? 'Remove': 'Add'}} Fav</button>
+        <ul v-if="visible">
+            <li>Phone: {{phoneNumber}}</li>
+            <li>Email: {{emailAddress}}</li>
+        </ul>
+    </li>
+</template>
+<script>
+export default {
+    props: {
+        id:{
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+        }
+    },
+    emits: ['toggle-fav'],
+    data(){
+        return{
+            visible: false,
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.visible= !this.visible
+        },
+        toggleFav() {
+            this.$emit('toggle-fav', this.id)
+        }
+    }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+</style>
